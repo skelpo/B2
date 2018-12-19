444e6d8e5efe74799994c3b2e3e55b7dede5ef7d
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-12-19 16:11:24 -0600

    Moved B2Auth struct to Auth.swift file

---
+++ Sources/B2/Auth.swift
@@ -0,0 +1,8 @@
+internal struct B2Auth: Codable {
+    let absoluteMinimumPartSize: Int
+    let accountId: String
+    let apiUrl: String
+    let authorizationToken: String
+    let downloadUrl: String
+    let recommendedPartSize: Int
+}
