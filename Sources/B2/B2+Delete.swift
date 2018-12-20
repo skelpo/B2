aa9ddda2e9b680a54ece9123ba40b835f86e2413
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-12-20 07:25:31 -0600

    Added URL to request in B2.delete(file:id:) method

--- Sources/B2/B2+Delete.swift
+++ Sources/B2/B2+Delete.swift
@@ -4,11 +4,16 @@ extension B2 {
     public func delete(file name: String, id: String) -> Future<Void> {
         return self.authorize().flatMap { auth in
             let body = try JSONEncoder().encode(["fileName": name, "fileId": id])
+            let path = "https://api.backblazeb2.com/b2api/v2/b2_delete_file_version"
+            guard let url = URL(string: path) else {
+                throw Abort(.internalServerError, reason: "Unable to create URL from path `\(path)`")
+            }
             
             var http = HTTPRequest()
             http.body = HTTPBody(data: body)
             http.headers = ["Authorization": auth.authorizationToken]
             http.method = .DELETE
+            http.url = url
             
             let request = Request(http: http, using: self.client.container)
             return self.client.send(request).transform(to: ())
